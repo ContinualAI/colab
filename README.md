968275909cf158b8e83a7b62261eaceda27b7f48
Antonio Carta <antonio.carta@di.unipi.it>
2021-03-12 09:00:15 +0100

    ADD draft avalanche modules notebooks

--- README.md
+++ README.md
@@ -8,15 +8,41 @@ We are building an open-source, collaborative wiki at [continualai.org](http://c
 
 ---
 
-### How to run it
+## Content
 
+- **Intro to Continual Learning** [notebook](./notebooks/intro_to_continual_learning.ipynb)
+- **Intro to Deep Learning Frameworks** [notebook](./notebooks/intro_to_dl_frameworks.ipynb)
+- **Replay Strategy** [notebook](./notebooks/CL_via_simple_rehearsal.ipynb)
+- **Generative Replay** [notebook](./notebooks/intro_to_generative_replay.ipynb)
+
+### Avalanche
+*Avalanche is a new end-to-end library for Continual Learning, developed and mantained by Continual AI. [Check it out!](github.com/continualAI/avalanche)*
+
+- **Avalanche Installation** [notebook](./notebooks/avalanche/avalanche_install.ipynb)
+- **Coding From Scratch vs Avalanche** [notebook](./notebooks/avalanche/with_and_without_avalanche.ipynb)
+- **From Zero to Hero 1: Introduction** [notebook](./notebooks/avalanche/01_fzth_tutorial_indroduction.ipynb)
+- **From Zero to Hero 2: Models** [notebook](./notebooks/avalanche/02_fzth_tutorial_models.ipynb)
+- **From Zero to Hero 3: Benchmarks** [notebook](./notebooks/avalanche/03_fzth_tutorial_benchmarks.ipynb)
+- **From Zero to Hero 4: Training** [notebook](./notebooks/avalanche/04_fzth_training.ipynb)
+- **From Zero to Hero 5: Evaluation** [notebook](./notebooks/avalanche/05_fzth_evaluation.ipynb)
+- **From Zero to Hero 6: Logging** [notebook](./notebooks/avalanche/06_fzth_logging.ipynb)
+- **From Zero to Hero 7: Putting It All Together** [notebook](./notebooks/avalanche/07_fzth_all_together.ipynb)
+- **From Zero to Hero 8: Extending Avalanche** [notebook](./notebooks/avalanche/08_fzth_extending.ipynb)
+- **From Zero to Hero 9: Contributing** [notebook](./notebooks/avalanche/09_fzth_contribute.ipynb)
+
+## How to run it
+
+### Colab
 1. Go to [Google Colab](https://colab.research.google.com) and sign in!
 
 2. Go to the "*GitHub*" tab and type "*continualai*"
 
 3. Pick a notebook an run it! :-)
 
-### How to contribute
+### Locally
+*local environment coming soon*
+
+## How to contribute
 
 We have a detailed account of the contribution procedure and guidelines in the corresponding file: [CONTRIBUTING](CONTRIBUTING.md)
 
--- scripts/mnist.py
+++ scripts/mnist.py
@@ -3,13 +3,15 @@ from urllib import request
 import gzip
 import pickle
 from os import path
+from torchvision.datasets import MNIST
 
 filename = [
-["training_images","train-images-idx3-ubyte.gz"],
-["test_images","t10k-images-idx3-ubyte.gz"],
-["training_labels","train-labels-idx1-ubyte.gz"],
-["test_labels","t10k-labels-idx1-ubyte.gz"]
+    ["training_images", "train-images-idx3-ubyte.gz"],
+    ["test_images", "t10k-images-idx3-ubyte.gz"],
+    ["training_labels", "train-labels-idx1-ubyte.gz"],
+    ["test_labels", "t10k-labels-idx1-ubyte.gz"]
 ]
+
 
 # quick fix for https://github.com/pytorch/vision/issues/1938
 # yann.lecun.com server has moved under CloudFlare protection
@@ -18,38 +20,54 @@ def download_file(url, filename):
     opener.addheader('User-Agent', 'Mozilla/5.0')
     opener.retrieve(url, filename)
 
+
 def download_mnist():
     base_url = "http://yann.lecun.com/exdb/mnist/"
     for name in filename:
-        print("Downloading "+name[1]+"...")
-        download_file(base_url+name[1], name[1])
+        print("Downloading " + name[1] + "...")
+        download_file(base_url + name[1], name[1])
     print("Download complete.")
+
 
 def save_mnist():
     mnist = {}
     for name in filename[:2]:
         with gzip.open(name[1], 'rb') as f:
             tmp = np.frombuffer(f.read(), np.uint8, offset=16)
-            mnist[name[0]] = tmp.reshape(-1,1,28,28).astype(np.float32) / 255
+            mnist[name[0]] = tmp.reshape(-1, 1, 28, 28).astype(np.float32) / 255
     for name in filename[-2:]:
         with gzip.open(name[1], 'rb') as f:
             mnist[name[0]] = np.frombuffer(f.read(), np.uint8, offset=8)
     with open("mnist.pkl", 'wb') as f:
-        pickle.dump(mnist,f)
+        pickle.dump(mnist, f)
     print("Save complete.")
+
 
 def init():
     # Check if already downloaded:
     if path.exists("mnist.pkl"):
         print('Files already downloaded!')
-    else: # Download Dataset
+    else:  # Download Dataset
         download_mnist()
         save_mnist()
 
+    prev_mnist_urls = MNIST.resources
+    new_resources = [
+        ('https://storage.googleapis.com/cvdf-datasets/mnist/train-images-idx3-ubyte.gz', prev_mnist_urls[0][1]),
+        ('https://storage.googleapis.com/cvdf-datasets/mnist/train-labels-idx1-ubyte.gz', prev_mnist_urls[1][1]),
+        ('https://storage.googleapis.com/cvdf-datasets/mnist/t10k-images-idx3-ubyte.gz', prev_mnist_urls[2][1]),
+        ('https://storage.googleapis.com/cvdf-datasets/mnist/t10k-labels-idx1-ubyte.gz', prev_mnist_urls[3][1])
+    ]
+    MNIST.resources = new_resources
+    MNIST(path.join('data', 'mnist'), download=True)
+
+
 def load():
-    with open("mnist.pkl",'rb') as f:
+    with open("mnist.pkl", 'rb') as f:
         mnist = pickle.load(f)
-    return mnist["training_images"], mnist["training_labels"], mnist["test_images"], mnist["test_labels"]
+    return mnist["training_images"], mnist["training_labels"], \
+           mnist["test_images"], mnist["test_labels"]
+
 
 if __name__ == '__main__':
     init()
