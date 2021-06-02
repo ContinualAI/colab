286f07d19c54c7f3c286bf782b9cc53c3c09ce51
Vincenzo Lomonaco <vincenzo.lomonaco@outlook.com>
2021-06-02 10:50:30 +0200

    Create download_and_extract_core50_mini.sh
    Short script useful to run in background with "nohup" on google colab (may take 10 minutes or so)

---
+++ scripts/download_and_extract_core50_mini.sh
@@ -0,0 +1,5 @@
+echo "Starting CORe50-mini download..."
+wget http://vps.continualai.org/data/core50_32x32.zip
+echo "Starting to extract CORe50-mini zip file..."
+unzip core50_32x32.zip
+echo "Done."
