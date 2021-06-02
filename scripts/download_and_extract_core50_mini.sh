7af7aea935d45fd0de39c065975663e9430566e1
Vincenzo Lomonaco <vincenzo.lomonaco@outlook.com>
2021-06-02 11:24:57 +0200

    Update download_and_extract_core50_mini.sh
    added additional useful download links to core50-mini download script

--- scripts/download_and_extract_core50_mini.sh
+++ scripts/download_and_extract_core50_mini.sh
@@ -1,5 +1,11 @@
 echo "Starting CORe50-mini download..."
 wget http://vps.continualai.org/data/core50_32x32.zip
+wget https://vlomonaco.github.io/core50/data/batches_filelists.zip
+wget https://vlomonaco.github.io/core50/data/batches_filelists_NICv2.zip
+wget https://vlomonaco.github.io/core50/data/paths.pkl
+wget https://vlomonaco.github.io/core50/data/LUP.pkl
+wget https://vlomonaco.github.io/core50/data/labels.pkl
+wget https://vlomonaco.github.io/core50/data/labels2names.pkl
 echo "Starting to extract CORe50-mini zip file..."
 unzip core50_32x32.zip
 echo "Done."
